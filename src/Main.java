575ff385585a75630c7e4d37c33565ec9bf83929
ДашаY13 <dasha130289@rambler.ru>
2021-10-06 17:04:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000 ;
+        int translation = 500_000_000;
+        int finite = balance + translation;
+        System.out.println(finite);
+    }
+}
